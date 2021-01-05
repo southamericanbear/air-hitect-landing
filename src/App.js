8c8d1eb14deae81ce79a7404461e01051be65cb6
alan Dittler <dittler.a@gmail.com>
2021-01-05 12:00:26 -0300

    about us finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import Navbar from "./parts/Navbar";
 import Home from "./parts/Home";
+import AboutUs from "./parts/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/parts/AboutUs.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function AboutUs() {
+  return (
+    <div className="about-us">
+      <div className="title-about-us">
+        <span>about us</span>
+      </div>
+      <main>
+        <div className="line-about-us"></div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          <span>Accusantium voluptatum beatae quibusdam</span> ex quod in
+          corrupti, eveniet neque unde ipsam, vel ipsa.
+        </p>
+        <div className="line-about-us"></div>
+        <div className="p-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ex
+            quia delectus sint numquam, modi reiciendis ipsam placeat odio
+            magni. Perferendis velit soluta vero.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            dolorum, quo, eius molestias laudantium facilis asperiores
+            reprehenderit eum, illo sunt dolores eveniet cupiditate veritatis?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            maxime illo iusto beatae voluptate quibusdam dolores officiis,
+            aperiam, nulla ipsa animi veritatis sed saepe!
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
